a6a9f3dfc8f3023b4768d72ab4711f8fd8f5003f
Theunis Cilliers <dev@theuniscilliers.com>
2020-04-03 08:07:09 +0200

    Good news update

--- src/app/modules/ui/sidebar/sidebar/sidebar.model.ts
+++ src/app/modules/ui/sidebar/sidebar/sidebar.model.ts
@@ -9,9 +9,9 @@ export interface NavItem {
 export const NavItems: NavItem[] = [
   { route: 'dashboard', label: 'Statistics' },
   { route: 'maps', label: 'Maps' },
-  { route: 'resources', label: 'Coronavirus Info', dev: true },
+  { route: 'resources', label: 'Information', dev: true },
   { route: 'testing', label: 'Testing Centres' },
-  { route: 'news', label: '"Good news" stories' },
+  { route: 'news', label: 'Good News' },
   { route: 'about', label: 'About' },
   { route: 'data', label: 'Data' }
 ];
